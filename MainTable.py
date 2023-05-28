62383ab829d7aea8d937a844c42729fdc49e8f12
motich351 <motih.iss1993@gmail.com>
2023-05-28 15:58:18 +0500

    Реализовать у работника функцию и удаления. Доработать поиск по адресу

--- MainTable.py
+++ MainTable.py
@@ -14,36 +14,6 @@ from ui_table import Ui_MainWindow
 from PyQt5.QtCore import Qt, QModelIndex
 
 
-class WorkerAdder(QDialog, Ui_WorkerAdd):
-    def __init__(self, session):
-        super().__init__()
-        self.setupUi(self)
-
-        self.session = session
-        self.worker = None
-        self.lineFIOWorker.setText("")
-        self.linePassportWorker_2.setText("")
-        self.lineSalaryWorker.setText("")
-        self.linePassportWorker.setText("")
-
-        self.ChooseRankWorker.addItems(['Админ', 'Уборщик', 'Начальник'])
-        shops = self.session.query(Shop).all()
-        for shop in shops:
-            self.ChooseWorkerPoint.addItem(shop.address, shop.id)
-
-        self.SaveWorker.clicked.connect(self.save_data)
-
-    def save_data(self):
-        self.worker = Worker(
-            fullname=self.lineFIOWorker.text(),
-            jobrank=self.ChooseRankWorker.currentText(),
-            phone=self.linePassportWorker_2.text(),
-            salary=self.lineSalaryWorker.text(),
-            passport=self.linePassportWorker.text(),
-            shop_id=(self.ChooseWorkerPoint.currentData())
-        )
-        self.accept()
-
 
 class TableViewer(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -57,8 +27,8 @@ class TableViewer(QMainWindow, Ui_MainWindow):
         self.session = create_session()
         self.WorkerUpdate.clicked.connect(self.load_table)
         self.tableWorker.doubleClicked.connect(self.update_worker)
-        #self.AddWorker.clicked.connect(self.update_worker)
         self.AddWorker.clicked.connect(self.add_worker)
+        self.SearchWorker.clicked.connect(self.search_worker)
 
 
     def add_worker(self):
@@ -134,6 +104,32 @@ class TableViewer(QMainWindow, Ui_MainWindow):
                 worker.passport = self.tableWidget.item(row, col).text()
                 self.session.commit()
 
+    def search_worker(self):
+        search_text = self.WorkerLine.text()
+        if not search_text:
+            self.load_table()  # Display all workers if search input is empty
+            return
+
+        workers = self.session.query(Worker).filter(
+            (Worker.fullname.ilike(f"%{search_text}%")) |
+            (Worker.phone.ilike(f"%{search_text}%")) |
+            (Worker.passport.ilike(f"%{search_text}%")) |
+            (Worker.jobrank.ilike(f"%{search_text}%")) |
+            (Worker.salary.ilike(f"%{search_text}%")) |
+            (Shop.address.ilike(f"%{search_text}%"))
+        ).all()
+
+        self.tableWorker.setRowCount(0)
+        for row, worker in enumerate(workers):
+            self.tableWorker.insertRow(row)
+            self.tableWorker.setItem(row, 0, QTableWidgetItem(str(worker.id)))
+            self.tableWorker.setItem(row, 1, QTableWidgetItem(worker.fullname))
+            self.tableWorker.setItem(row, 2, QTableWidgetItem(worker.jobrank))
+            self.tableWorker.setItem(row, 3, QTableWidgetItem(str(worker.phone)))
+            self.tableWorker.setItem(row, 4, QTableWidgetItem(str(worker.salary)))
+            self.tableWorker.setItem(row, 5, QTableWidgetItem(str(worker.passport)))
+            self.tableWorker.setItem(row, 6, QTableWidgetItem(str(worker.shop.address)))
+
 
 class WorkerUpdater(QDialog, Ui_WorkerAdd):
     def __init__(self, worker_id):
@@ -171,6 +167,37 @@ class WorkerUpdater(QDialog, Ui_WorkerAdd):
         self.worker.shop_id = shop_id
         self.session.commit()
         self.close()
+
+
+class WorkerAdder(QDialog, Ui_WorkerAdd):
+    def __init__(self, session):
+        super().__init__()
+        self.setupUi(self)
+
+        self.session = session
+        self.worker = None
+        self.lineFIOWorker.setText("")
+        self.linePassportWorker_2.setText("")
+        self.lineSalaryWorker.setText("")
+        self.linePassportWorker.setText("")
+
+        self.ChooseRankWorker.addItems(['Админ', 'Уборщик', 'Начальник'])
+        shops = self.session.query(Shop).all()
+        for shop in shops:
+            self.ChooseWorkerPoint.addItem(shop.address, shop.id)
+
+        self.SaveWorker.clicked.connect(self.save_data)
+
+    def save_data(self):
+        self.worker = Worker(
+            fullname=self.lineFIOWorker.text(),
+            jobrank=self.ChooseRankWorker.currentText(),
+            phone=self.linePassportWorker_2.text(),
+            salary=self.lineSalaryWorker.text(),
+            passport=self.linePassportWorker.text(),
+            shop_id=(self.ChooseWorkerPoint.currentData())
+        )
+        self.accept()
 
 
 def my_exception_hook(exctype, value, traceback):
